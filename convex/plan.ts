7307492417cced952e258a818e110b946251e367
oferitz <ofer.webdev@gmail.com>
2024-03-23 11:49:27 +0200

    fix

--- convex/plan.ts
+++
@@ -1,9 +0,0 @@
-import { authQuery } from '~/convex/utils'
-import { getManyFrom } from 'convex-helpers/server/relationships'
-
-export const getMyUserPlans = authQuery({
-  handler: async ({ db, user }) => {
-    if (!user) return []
-    return await getManyFrom(db, 'plans', 'by_userId', user._id)
-  }
-})
