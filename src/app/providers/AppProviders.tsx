c5d425883840256314cdf27385d278c8878033fd
oferitz <ofer.webdev@gmail.com>
2024-04-26 09:29:53 +0300

    upgrade all deps

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,11 +1,11 @@
 import type React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
+import { shadesOfPurple } from '@clerk/themes'
 import ClientProviders from '@/app/providers/ClientProviders'
 
 export default function AppProviders({ children }: React.PropsWithChildren) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
+    <ClerkProvider appearance={{ baseTheme: shadesOfPurple }}>
       <ClientProviders>{children}</ClientProviders>
     </ClerkProvider>
   )
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
-import { authMiddleware } from '@clerk/nextjs'
+import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ['/', '/unsubscribe'],
-  ignoredRoutes: ['/api/sendEmail']
+const isProtectedRoute = createRouteMatcher(['/hs(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
 })
 
 export const config = {
